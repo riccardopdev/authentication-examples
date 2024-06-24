5ad59499a5cf25385a06a0ac22c4587a75449b24
Riccardo P <riccardop@outlook.com>
2024-06-24 17:31:50 +1000

    Create basic express server for node-react-authentication

--- node-react-authentication/server/index.js
+++ node-react-authentication/server/index.js
@@ -1,1 +1,10 @@
-console.log('Server');
+const express = require('express');
+const app = express();
+
+const users = ['user1'];
+
+app.get('/users', (req, res) => {
+  res.json(users);
+})
+
+app.listen(3010);
