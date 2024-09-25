10fe0bdb229b86e3f803161bebda94373943daaa
Riccardo P <riccardop@outlook.com>
2024-09-25 16:43:21 +1000

    node-react-authentication - Added bcrypt, created basic endpoints to get all users, create a new user with hashed password, login a user while comparing plain text password with hashed password

--- node-react-authentication/server/index.js
+++ node-react-authentication/server/index.js
@@ -1,10 +1,61 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
 const app = express();
 
-const users = ['user1'];
+//This will allow to accept json data and populate the req.body object with parameters sent with POST requests
+app.use(express.json());
 
+//Our database of users
+const USERS = [];
+
+//Get all USERS
 app.get('/users', (req, res) => {
-  res.json(users);
-})
+  res.json(USERS);
+});
+
+//Create new user
+app.post('/users', async (req, res) => {
+
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    const user = { name: req.body.name, password: hashedPassword };
+
+    USERS.push(user);
+    res.status(201).send();
+
+    console.log('Passowrd', req.body.password)
+    console.log('Salt', salt);
+    console.log('Hashed Password', hashedPassword);
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send();
+  }
+});
+
+//Login existing user
+app.post('/users/login', async (req, res) => {
+  const user = USERS.find((user) => user.name === req.body.name);
+
+  if (user == null) {
+    return res.status(400).send('Cannot find user');
+  }
+
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success');
+    } else {
+      res.send('Wrong password');
+    }
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).send();
+  }
+});
 
 app.listen(3010);
